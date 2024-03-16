1500e98fa25e68c2d0ab0850703bbba44b475231
Evgenii Fedotov (evfedoto) <evfedoto@cisco.com>
2024-03-16 12:17:17 +0100

    srv refactoring

--- srv/srv_test.go
+++ srv/srv_test.go
@@ -13,21 +13,10 @@ func TestSrv(t *testing.T) {
 	srvresults.ForDomain("akbank.com")
 
 	for _, res := range *srvresults {
-		
-		if res.Children != nil {
-			fmt.Println("=================")
-			fmt.Println(res.Children)
-			if res.Children[0].Children != nil {
-				fmt.Println("=================")
-				fmt.Println(res.Children[0].Children[0])
-				if res.Children[0].Children[0].Children != nil {
-					fmt.Println("=================")
-					fmt.Println(res.Children[0].Children[0].Children[0])
-				}
-			}
-			
-		}
-		
+		fmt.Println("res: ", res.Fqdn)
+		for i, cert := range res.CertsChain{
+			fmt.Println("cert ", i, cert)
+		} 
 		
 	}
 }
